f24ab141b7bc49c7fb3391af8d7cabf437b5122b
Miguel Casqueira <miguel@balena.io>
2020-05-23 15:11:48 -0400

    install user defined packages

--- UserSetup.sh
+++ UserSetup.sh
@@ -6,19 +6,22 @@ xdg-user-dirs-update
 # Copy configuration files to your user folder.
 cp -rT home/mig ~/
 
-# Launching mako
+# Setting mako executable
 chmod +x ~/.config/mako/launch.sh
 
+# Setting wallpaper with wal
 if [[ ! -e ~/.config/mako/config ]]; then
-	# Setting wallpaper with wal
 	wal -i /home/mig/dotfiles/default.png
 	ln -s ~/.cache/wal/mako.conf ~/.config/mako/config
 fi
 
+# Installing the required icon pack so waybar functions correctly.
 if [[ ! -d "ttf-material-design-icons-git" ]]; then
-	# Installing the required icon pack so waybar functions correctly.
 	cd ~/dotfiles
 	git clone https://aur.archlinux.org/ttf-material-design-icons-git.git
 	cd ttf-material-design-icons-git
 	makepkg -si
 fi
+
+# Installing user packages
+sudo pacman -S --needed $(comm -12 <(pacman -Slq|sort) <(sort pacmanPkg.list) )
