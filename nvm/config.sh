c89dd70507a86725649b9c45846f0d3b9553ebcc
20k-ultra <mig@xgho.st>
2022-01-02 00:25:42 -0500

    improve nvm install

---
+++ nvm/config.sh
@@ -0,0 +1,4 @@
+source /usr/share/nvm/init-nvm.sh
+
+# Install node 14
+nvm install 14
--- zznvm/install.sh
+++
@@ -1,5 +0,0 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
-source ~/.zshrc
-
-nvm install 14
