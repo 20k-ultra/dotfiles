99542bf4822fc3e3da18214113c014bc4e7463d0
20k-ultra <mig@xgho.st>
2022-01-04 14:34:35 -0500

    added way to automate NPM package installations

--- nvm/config.sh
+++ nvm/config.sh
@@ -1,4 +1,19 @@
+#/bin/sh
+
+# Initialize nvm so we can use it
 source /usr/share/nvm/init-nvm.sh
 
-# Install node 14
-nvm install 14
+NODE_VERSION=16.13.1
+
+# Install node 16
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION # Set default in case other versions were previously installed 
+nvm use $NODE_VERSION # Set $NODE_VERSION to active version
+
+# Install NPM packages
+if [[ $(cat "$HOME/dotfiles/nvm/npmPkg.list" | wc -l) > 0 ]]; then
+	echo "Installing $(cat $HOME/dotfiles/nvm/npmPkg.list)"
+	npm install -g $(cat $HOME/dotfiles/nvm/npmPkg.list)
+else
+	echo "No packages to install."
+fi
