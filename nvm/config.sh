980636663618d03182f7ceb451d838371702138c
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-01-04 23:39:07 -0500

    remove ability to install npm packages in nvm topic

--- nvm/config.sh
+++ nvm/config.sh
@@ -9,11 +9,3 @@ NODE_VERSION=16.13.1
 nvm install $NODE_VERSION
 nvm alias default $NODE_VERSION # Set default in case other versions were previously installed 
 nvm use $NODE_VERSION # Set $NODE_VERSION to active version
-
-# Install NPM packages
-if [[ $(cat "$HOME/dotfiles/nvm/npmPkg.list" | wc -l) > 0 ]]; then
-	echo "Installing $(cat $HOME/dotfiles/nvm/npmPkg.list)"
-	npm install --global --production $(cat $HOME/dotfiles/nvm/npmPkg.list)
-else
-	echo "No packages to install."
-fi
