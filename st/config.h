8b1fceb62498f69888c285ea265ba995a6fb02e4
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-01-09 00:06:40 -0500

    st: increased font size

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=21:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=25:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 2;
 
