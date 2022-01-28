6a9c82b97514a38e00fc5ccff2086efa6d0be3d6
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-01-27 23:35:37 -0500

    system: add Apple touchpad 2 setup

--- xorg/config.sh
+++ xorg/config.sh
@@ -2,4 +2,5 @@
 
 # Symlink xorg config for nvidia drivers
 sudo ln -sf $HOME/dotfiles/xorg/20-nvidia.conf /etc/X11/xorg.conf.d
+sudo ln -sf $HOME/dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
 sudo ln -sf $HOME/dotfiles/xorg/xorg.conf /etc/X11
