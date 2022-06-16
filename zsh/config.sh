be60cc10aaaa98dc54c9b3f43034164ff8b5d806
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-06-16 00:21:02 -0400

    zsh: remove antigen from managing plugins
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- zsh/config.sh
+++ zsh/config.sh
@@ -2,3 +2,11 @@
 
 # Set zsh as shell
 sudo chsh -s $(which zsh) $(whoami)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
