f05432f295ca83b458b6ff2f0e3a8a8b403e3f7e
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-09-21 11:40:43 -0400

    nvim: setup null-ls for shellcheck

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+local null_ls = require "null-ls"
+
 local config = {
   -- Add plugins
   plugins = {
@@ -18,6 +20,28 @@ local config = {
     packer = {
       compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
     },
+    ["null-ls"] = function(config)
+      -- Check supported formatters and linters
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      config.sources = {
+        -- Set sources here
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.formatting.shfmt,
+      }
+      -- set up null-ls's on_attach function
+      -- NOTE: You can remove this on attach function to disable format on save
+      config.on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            pattern = "<buffer>",
+            callback = vim.lsp.buf.formatting_sync,
+          })
+        end
+      end
+      return config -- return final config table to use in require("null-ls").setup(config)
+    end
   },
 
   colorscheme = "sonokai",
