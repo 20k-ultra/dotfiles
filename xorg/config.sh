6f40ead9e6c566a8944ecf090205552c6ef1a786
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-10-04 13:58:07 -0400

    xorg: disable trackpad

--- xorg/config.sh
+++ xorg/config.sh
@@ -4,3 +4,6 @@
 sudo ln -sf $HOME/dotfiles/xorg/20-nvidia.conf /etc/X11/xorg.conf.d
 sudo ln -sf $HOME/dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
 sudo ln -sf $HOME/dotfiles/xorg/xorg.conf /etc/X11
+
+# Disable trackpad
+xinput set-prop "Synaptics TM3512-010" "Device Enabled" 0
