b458817bb58e4852dc21cb1b0b46ca300af0b716
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-10-29 12:13:18 -0400

    system: add rotate keybind for mpv

--- system/config.sh
+++ system/config.sh
@@ -1,2 +1,5 @@
 # Add user to video group so we can control screen brightness
-sudo usermod -a -G video $(whoami) 
+sudo usermod -a -G video $(whoami)
+
+mkdir -p ~/.mpv
+ln -s ~/dotfiles/system/mpv/input.conf ~/.mpv/input.conf
