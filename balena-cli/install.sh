810076550cd6e8366bd603d0cd456dd2ecca351a
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-01-13 14:05:31 -0500

    Update balena-cli to v15.0.1
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- balena-cli/install.sh
+++ balena-cli/install.sh
@@ -1,16 +1,20 @@
-#/bin/sh
+#!/bin/sh
 
 # Exit early if already installed
-command -v balena >/dev/null && { echo "balena-cli already installed...skipping"; exit 0; } 
+command -v balena >/dev/null && {
+	echo "balena-cli already installed...skipping"
+	exit 0
+}
 
-# Download CLI 
-curl -Lo /tmp/balena-cli.zip "https://github.com/balena-io/balena-cli/releases/download/v13.1.6/balena-cli-v13.1.6-linux-x64-standalone.zip"
+# Download CLI
+VERSION=15.0.1
+curl -Lo /tmp/balena-cli.zip "https://github.com/balena-io/balena-cli/releases/download/v$VERSION/balena-cli-v$VERSION-linux-x64-standalone.zip"
 
 # Install unzip
 paru -S --needed unzip
 
 # Extract source
-unzip -x /tmp/balena-cli.zip -d $HOME/.local
+unzip -x /tmp/balena-cli.zip -d "$HOME/.local"
 
 # Symlink exectuable to bin
-ln -sF $HOME/.local/balena-cli/balena $HOME/.local/bin
+ln -sF "$HOME/.local/balena-cli/balena" "$HOME/.local/bin"
