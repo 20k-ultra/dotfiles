6eb24ea46d0ba4c9841b6355fe9e8e3d2037a5ea
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-02-04 16:23:24 -0500

    zsh: added function to print times info for curl request
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,12 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Set zsh as shell
-sudo chsh -s $(which zsh) $(whoami)
+# Install and set zsh as shell
+if command -v zsh >/dev/null; then
+	sudo chsh -s "$(which zsh)" "$(whoami)"
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	if [ ! -d "$HOME/.oh-my-zsh" ]; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
+else
+	echo "zsh is not installed, please install it before continuing"
+fi
 
 # Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-completions" ]; then
+	git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+fi
