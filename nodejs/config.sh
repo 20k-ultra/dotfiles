6c38aed543d67c75047295496031e37236d3ed75
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-03-09 11:51:48 -0500

    nodejs: install pnpm

--- nodejs/config.sh
+++ nodejs/config.sh
@@ -5,7 +5,10 @@ source /usr/share/nvm/init-nvm.sh
 
 NODE_VERSION=16.13.1
 
+# Install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
 # Install node 16
 nvm install $NODE_VERSION
-nvm alias default $NODE_VERSION # Set default in case other versions were previously installed 
-nvm use $NODE_VERSION # Set $NODE_VERSION to active version
+nvm alias default $NODE_VERSION # Set default in case other versions were previously installed
+nvm use $NODE_VERSION           # Set $NODE_VERSION to active version
