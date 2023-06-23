64c985f3b00c008006707bfaa8c9b782e50f4e07
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-06-23 18:19:53 -0400

    zsh: revert back to OMZ for now
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-
 # Install and set zsh as shell
 if command -v zsh >/dev/null; then
 	sudo chsh -s "$(which zsh)" "$(whoami)"
+
+	if [ ! -d "$HOME/.oh-my-zsh" ]; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
 else
 	echo "zsh is not installed, please install it before continuing"
 fi
 
-ZDOTDIR=~/.config/zsh
-
-if [ ! -d "$ZDOTDIR" ]; then
-	echo "Setting up zdot config"
-	git clone https://github.com/getantidote/zdotdir $ZDOTDIR
+# Plugins
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 fi
 
-if [ ! -d "$ZDOTDIR/.antidote" ]; then
-	echo "Setting up antidote"
-	git clone --depth=1 https://github.com/mattmc3/antidote.git "$ZDOTDIR/.antidote"
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 fi
 
-ln -sf "$ZDOTDIR/.zshenv" ~/.zshenv
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-completions" ]; then
+	git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-vi-mode" ]; then
+	git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/plugins/zsh-vi-mode
+fi
